802f3eeb94987b003c78893e71b2305b5ae34d1f
Adil Shabab <adilshabab2004@gmail.com>
2023-02-13 20:20:32 +0530

    recent portfolio section completed in home page

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+const items = document.querySelectorAll('.item')
+
+
+const expand = (item, i) => {
+
+
+  items.forEach((it, ind) => {
+    if (i === ind) return
+    it.clicked = false
+  })
+  gsap.to(items, {
+    width: item.clicked ? '18%' : '12%',
+    duration: 2,
+    ease: 'elastic(1, .6)'
+  })
+  
+  item.clicked = !item.clicked
+  gsap.to(item, {
+    width: item.clicked ? '38%' : '15.5%',
+    duration: 2.5,
+    ease: 'elastic(1, .3)'
+  })
+}
+
+items.forEach((item, i) => {
+  item.clicked = false
+  item.addEventListener('click', () => expand(item, i))
+})
